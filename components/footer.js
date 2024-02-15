c52a50e0fba1d56531bdb3aa5396df0404de9ffd
itc-s23014 <s23014@std.it-college.ac.jp>
2024-02-15 08:00:12 +0000

    121まで完

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,15 @@
+import Container from 'components/container'
 import Logo from 'components/logo'
+import styles from 'styles/footer.module.css'
+const Footer = () => (
+  <footer className={styles.wrapper}>
+    <Container>
+      <div className={styles.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
+    </Container>
+  </footer>
+)
 
-const Footer = () => {
-  return (
-    <footer>
-      <Logo />
-    </footer>
-  )
-}
 export default Footer
