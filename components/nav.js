be06d90e40ece07c7c72141ba3b68febbee43428
itc-s23014 <s23014@std.it-college.ac.jp>
2024-02-22 05:04:25 +0000

    chapter10.3 fin
    useStateを使ってハンバーガーメニューを作成した

--- components/nav.js
+++ components/nav.js
@@ -1,19 +1,49 @@
 import Link from 'next/link'
 import styles from 'styles/nav.module.css'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavIsOpen(prev => !prev)
+  }
+
+  const closeNav = () => {
+    setNavIsOpen(false)
+  }
   return (
-    <nav>
-      <button className={styles.btn}>MENU</button>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>{`
+          @media (max-width: 767px) {
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            }
+          }
+        `}</style>
+      )}
+      <button className={styles.btn} onClick={toggleNav}>
+        <span className={styles.bar}></span>
+        <span className='sr-only'>MENU</span>
+      </button>
       <ul className={styles.list}>
         <li>
-          <Link href='/'>Home</Link>
+          <Link onClick={closeNav} href='/'>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href='/about'>About</Link>
+          <Link onClick={closeNav} href='/about'>
+            About
+          </Link>
         </li>
         <li>
-          <Link href='/blog'>Blog</Link>
+          <Link onClick={closeNav} href='/blog'>
+            Blog
+          </Link>
         </li>
       </ul>
     </nav>
